1a0b1e2e3ed342a99b8f1a75133c479a6b2f7414
shintana <39305311+shintana@users.noreply.github.com>
2021-12-15 20:43:37 +0700

    Update test.sh

--- .github/test.sh
+++ .github/test.sh
@@ -1,47 +1,8 @@
-generate_post_data()
-{
-  cat <<EOF
-{
-	"channel":"C02LV4R4X4Z",
-	"blocks": [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "${{ github.event.pull_request.title }} <@${{ steps.get-contributor-slack.outputs.contributorID }}"
-			}
-		},
-		{
-			"type": "section",
-			"fields": [
-				{
-					"type": "plain_text",
-					"text": "*Reviewer*"
-				},
-				{
-					"type": "plain_text",
-					"text": "<@${{ steps.get-reviewer-slack.outputs.reviewerID }}>"
-				},
-				{
-					"type": "plain_text",
-					"text": "*Status*"
-				},
-				{
-					"type": "plain_text",
-					"text": "`Need Approval`"
-				}
-			]
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "<${{ github.event.pull_request.html_url }} | View Pull Request>"
-			}
-		}
-	]
-}
-EOF
-}
-curl -i -H "Content-type:application/json" -H "Authorization: Bearer $SLACK_BOT_TOKEN" -X POST --data "$(generate_post_data)" "https://slack.com/api/chat.postMessage"
+# SLACK_BOT_TOKEN="${{ secrets.SLACK_BOT_TOKEN }}"
+data='{"channel":"C02LV4R4X4Z","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"${{ github.event.pull_request.title }} <@${{ steps.get-contributor-slack.outputs.contributorID }}>"}},{"type":"section","fields":[{"type":"mrkdwn","text":"*Reviewer*"},{"type":"mrkdwn","text":"<@${{ steps.get-reviewer-slack.outputs.reviewerID }}>"},{"type":"mrkdwn","text":"*Status*"},{"type":"mrkdwn","text":"`Need Approval`"}]},{"type":"section","text":{"type":"mrkdwn","text":"<${{ github.event.pull_request.html_url }} | View Pull Request>"}}]}'
 
+curl -X POST \
+     -H "Content-type: application/json" \
+     -H "Authorization: Bearer $SLACK_BOT_TOKEN" \
+     -d "$data" \
+     https://slack.com/api/chat.postMessage
