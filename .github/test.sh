ed4f714ebb02f16b6ddd097c5ac08e02a6fb01c3
shintana <39305311+shintana@users.noreply.github.com>
2021-12-15 16:55:11 +0700

    Create test.sh

---
+++ .github/test.sh
@@ -0,0 +1,45 @@
+generate_post_data()
+{
+  cat <<EOF
+{
+	"channel":"C02LV4R4X4Z",
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "${{ github.event.pull_request.title }} <@${{ steps.get-contributor-slack.outputs.contributorID }}"
+			}
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "plain_text",
+					"text": "*Reviewer*"
+				},
+				{
+					"type": "plain_text",
+					"text": "<@${{ steps.get-reviewer-slack.outputs.reviewerID }}>"
+				},
+				{
+					"type": "plain_text",
+					"text": "*Status*"
+				},
+				{
+					"type": "plain_text",
+					"text": "`Need Approval`"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "<${{ github.event.pull_request.html_url }} | View Pull Request>"
+			}
+		}
+	]
+}
+EOF
+}
