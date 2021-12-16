1b45d7a890a3a98976c06423e2ace03004383208
shintana <39305311+shintana@users.noreply.github.com>
2021-12-16 09:07:40 +0700

    Update test.sh

--- .github/test.sh
+++ .github/test.sh
@@ -8,9 +8,9 @@ fi
 # data='{"channel":"C02LV4R4X4Z","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"${{ github.event.pull_request.title }} <@${{ steps.get-contributor-slack.outputs.contributorID }}>"}},{"type":"section","fields":[{"type":"mrkdwn","text":"*Reviewer*"},{"type":"mrkdwn","text":"<@${{ steps.get-reviewer-slack.outputs.reviewerID }}>"},{"type":"mrkdwn","text":"*Status*"},{"type":"mrkdwn","text":"`Need Approval`"}]},{"type":"section","text":{"type":"mrkdwn","text":"<${{ github.event.pull_request.html_url }} | View Pull Request>"}}]}'
 # data='{"channel":"C02LV4R4X4Z","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"'$PR_TITLE' <@'$CONTRIBUTOR_ID'>"}},{"type":"section","fields":[{"type":"mrkdwn","text":"*Reviewer*"},{"type":"mrkdwn","text":"<@'$REVIEWER_ID'>"},{"type":"mrkdwn","text":"*Status*"},{"type":"mrkdwn","text":"`Need Approval`"}]},{"type":"section","text":{"type":"mrkdwn","text":"<'$PR_LINK' | View Pull Request>"}}]}'
 # data='{"channel":"C02LV4R4X4Z","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"'${{github.event.pull_request.title}}' <@$CONTRIBUTOR_ID>"}},{"type":"section","fields":[{"type":"mrkdwn","text":"*Reviewer*"},{"type":"mrkdwn","text":"<@$REVIEWER_ID>"},{"type":"mrkdwn","text":"*Status*"},{"type":"mrkdwn","text":"`Need Approval`"}]},{"type":"section","text":{"type":"mrkdwn","text":"<$PR_LINK | View Pull Request>"}}]}'
-data='{"channel":"C02LV4R4X4Z","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"'$PR_TITLE'"}}]}'
+data='{"channel":"C02LV4R4X4Z","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"'$CONTRIBUTOR_ID'"}}]}'
 
-echo "outputnya: $PR_TITLE"
+echo "outputnya: $CONTRIBUTOR_ID"
 curl -X POST \
      -H "Content-type: application/json" \
      -H "Authorization: Bearer $SLACK_BOT_TOKEN" \
